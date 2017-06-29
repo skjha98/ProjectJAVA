5dc005f1364d62c2ce3d54b49a70f76390a4ae8c
skjha98 <skjha98@gmail.com>
2017-06-29 07:06:37 +0530

    Use of Scanner. Project v1.1 Compiling & Executing

--- Project.java
+++ Project.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class Project{
-  public static void main(){
-    System.out.println("Hello JAVA!!!");
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int a = input.nextInt();
+    System.out.println("Good Morning SJ" + a);
   }
 }
